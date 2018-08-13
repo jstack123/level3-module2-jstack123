9c50f89b03e4b025b630442f0c4d24d0866a4c36
jstack123 <league@iMac.attlocal.net>
2018-08-12 17:32:14 -0700

    finished more and more

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -3,7 +3,78 @@ package _02_More_Algorithms;
 import java.util.List;
 
 public class Algorithms {
+
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		int index = 0;
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).equals("cracked")) {
+				index = i;
+				break;
+			}
+		}
+		return index;
 	}
+
+	public static int countPearls(List<Boolean> oysters) {
+		int pearls = 0;
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i) == true) {
+				pearls += 1;
+			}
+		}
+		return pearls;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		double tallest = peeps.get(0);
+
+		for (int i = 0; i < peeps.size(); i++) {
+
+			if (peeps.get(i) > tallest) {
+				tallest = peeps.get(i);
+			}
+		}
+		return tallest;
+	}
+
+	public static String findLongestWord(List<String> words) {
+		String longest = words.get(0);
+
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > longest.length()) {
+				longest = words.get(i);
+			}
+		}
+		return longest;
+	}
+
+	public static boolean containsSOS(List<String> message1) {
+
+		for (int i = 0; i < message1.size(); i++) {
+			if (message1.get(i).contains("... --- ...")) {
+				System.out.println("hi");
+				return true;		
+			}
+		}
+		return false;
+	}
+
+	public static List<Double> sortScores(List<Double> results) {
+	double temp;
+		for (int i = 0; i < results.size(); i++) {
+			for (int j = i+1; j < results.size()-1; j++) {
+				double one = results.get(i);
+				double two = results.get(j);
+				if (one>two) {
+					temp = two;
+					two = one;
+					one = temp;
+				}
+			}
+		}
+		
+		return results;
+	}
+	
+
 }
--- src/_02_More_Algorithms/_00_MoreAndMoreTests.java
+++ src/_02_More_Algorithms/_00_MoreAndMoreTests.java
@@ -6,57 +6,98 @@ import org.junit.Test;
 
 /**
  * Uncomment each test and then write the method to make it pass.
- * **/
+ **/
 
 public class _00_MoreAndMoreTests {
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+	private String multiply(int a, int b) {
+		int answer = a * b;
+		String answerWord = "" + a + " x " + b + " = " + answer;
+		return answerWord;
+	}
 
+	private boolean isPrime(int x) {
+		int numFactors = x-1;
+		for (int i = numFactors; i > 1 ; i--) {
+			if (x%i==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private boolean isSquare(int y) {
+		int nums = y;
+		
+		for (int i = nums; i > 0; i--) {
+			if (y/i==i) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean isCube(int z) {
+		int nums = z;
+		
+		for (int i = nums; i > 0; i--) {
+			double second = z/i;
+			if (second/i==i) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	@Test
+	public void MultTest() {
+
+		assertEquals("10 x 0 = 0", multiply(10, 0));
+		assertEquals("10 x 10 = 100", multiply(10, 10));
+		assertEquals("8 x 11 = 88", multiply(8, 11));
+	}
+
+	@Test
+	public void PrimeTest() {
+
+		assertTrue(isPrime(3));
+		assertTrue(isPrime(5));
+		assertTrue(isPrime(541));
+		assertFalse(isPrime(4));
+		assertFalse(isPrime(12));
+		assertFalse(isPrime(527));
+
+	}
+	
+	 @Test
+	 public void SquareTest() {
+	
+	 assertTrue(isSquare(4));
+	 assertTrue(isSquare(144));
+	 assertTrue(isSquare(64));
+	 assertTrue(isSquare(10201));
+	 assertTrue(isSquare(1));
+	 assertFalse(isSquare(3));
+	 assertFalse(isSquare(22));
+	 assertFalse(isSquare(143));
+	
+	 }
+
+	
+	
+	 @Test
+	 public void CubeTest() {
+	
+	 assertTrue(isCube(27));
+	 assertTrue(isCube(216));
+	 assertTrue(isCube(729));
+	 assertTrue(isCube(1));
+	 assertFalse(isCube(3));
+	 assertFalse(isCube(22));
+	 assertFalse(isCube(143));
+	
+	 }
 
 
 }
